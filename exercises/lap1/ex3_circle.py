1e3f850952e363b18399e6d33ce2559cd7a1c8f4
TDKhoa2712 <khoatd2712@gmail.com>
2025-09-16 02:52:19 +0700

    Final my_leson1.ipynb

---
+++ exercises/lap1/ex3_circle.py
@@ -0,0 +1,7 @@
+import math
+print("Tinh chu vi va dien tich hinh tron")
+r = float(input("Moi ban nhap ban kinh r: "))
+cv = 2 * math.pi * r
+dt = math.pi * r * r
+print("Chu vi hinh tron la: %.2f" % cv)
+print("Dien tich hinh tron la: %.2f" % dt)
